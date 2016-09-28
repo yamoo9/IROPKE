9a95afb1a87dc6d2b824766c241828d7101c27f6
Jee Hoon <yamoo9@naver.com>
2016-09-28 20:25:00 +0900

    링크 클릭 이벤트 제어 part 1

--- JavaScript_CAMP/DAY07/js/events.js
+++ JavaScript_CAMP/DAY07/js/events.js
@@ -3,14 +3,45 @@
 (function(global){
   'use strict';
 
-  // .demo-nav 요소 내부의 <a> 요소를 클릭할 때,
-  // #print-select-link-content 요소의 콘텐츠 값이
-  // 선택된 <a> 요소의 콘텐츠 값으로 출력되도록 한다.
-  // 선택된 <a> 요소 선택된 상태를 시각적으로 표시하도록 설정한다.
-  // .selected 클래스 속성을 추가 한다.
-  // 단, 각 <a> 요소는 선택된 상태가 고유해야 한다.
+  var print_area           = null;
+  var demo_nav             = null;
+  var demo_nav_links       = null;
+  var demo_nav_remove_btns = null;
 
-  // .demo-nav 요소 내부의 <button> 요소를 클릭할 때,
-  // 선택된 <button>을 포함한 목록 항목(List Item)을 제거한다.
+  function initialization() {
+
+    // DOM 요소를 선택
+    print_area           = document.querySelector('#print-select-link-content');
+    demo_nav             = document.querySelector('.demo-nav');
+    demo_nav_links       = demo_nav.querySelectorAll('a');
+    demo_nav_remove_btns = demo_nav.querySelectorAll('.remove-item');
+
+    // .demo-nav 요소 내부의 <a> 요소를 클릭할 때,
+    for(var l=demo_nav_links.length, link; --l;) {
+      link = demo_nav_links[l];
+      link.onclick = printContent;
+    }
+    // .selected 클래스 속성을 추가 한다.
+    // 단, 각 <a> 요소는 선택된 상태가 고유해야 한다.
+
+    // .demo-nav 요소 내부의 <button> 요소를 클릭할 때,
+    // 선택된 <button>을 포함한 목록 항목(List Item)을 제거한다.
+  }
+
+  function printContent() {
+    var _link = this;
+    // 클릭된 <a> 요소가 포함하는 텍스트 노드 값을 가져와서 변수에 참조
+    // #print-select-link-content 요소의 콘텐츠 값이
+    // 선택된 <a> 요소의 콘텐츠 값으로 출력되도록 한다.
+    var _content = _link.firstChild.nodeValue;
+    print_area.firstChild.nodeValue = _content;
+    // 선택된 <a> 요소 선택된 상태를 시각적으로 표시하도록 설정한다.
+    var _changed_class = _link.getAttribute('class') + ' selected';
+    _link.setAttribute('class', _changed_class);
+    // Legacy Prevent Default Action
+    return false;
+  }
+
+  window.onload = initialization;
 
 })(this);
