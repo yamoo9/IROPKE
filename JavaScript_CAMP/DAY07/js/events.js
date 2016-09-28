f6e4eadf6f5fdacbf43293328876efc4cb04ecca
Jee Hoon <yamoo9@naver.com>
2016-09-28 22:26:12 +0900

    동적으로 콘텐츠를 추가하는 DOM API 방법 추가

--- JavaScript_CAMP/DAY07/js/events.js
+++ JavaScript_CAMP/DAY07/js/events.js
@@ -25,11 +25,13 @@
     Array.prototype.forEach.call(demo_nav_remove_btns, function(button, index) {
       button.onclick = removeItem;
     });
+
+    // 사용자가 입력 가능한 인풋 컴포넌트에 이벤트 바인딩
+    print_area.onkeydown = userInput;
   }
   // 이벤트 핸들러 printContent 정의
   function printContent() {
     printAssignContent.call(this);
-    removeSiblingsSelectedClass.call(this);
     assignClass.call(this);
     // 오래 전에 사용되던 브라우저 기본 동작 차단 방법
     return false;
@@ -45,6 +47,7 @@
   function assignClass() {
     var _has_selected_class = checkSelectedClass(this);
     if( _has_selected_class ) { return; }
+    removeSiblingsSelectedClass.call(this);
     var _pre_class = this.getAttribute('class') || '';
     var _changed_class = _pre_class + ' selected';
     this.setAttribute( 'class', _changed_class.trim() );
@@ -55,6 +58,7 @@
     _classes = !_classes ? [] : _classes.split(' ');
     var i=0, l=_classes.length;
     for (; i<l; i++) {
+      // console.log(_classes[i]);
       if ( _classes[i] === 'selected' ) { return true; }
     }
     return false;
@@ -76,6 +80,33 @@
   function removeItem() {
     var _parent = this.parentNode;
     _parent.parentNode.removeChild(_parent);
+  }
+  function userInput(event) {
+    var _target   = event.target;
+    var _key_code = event.keyCode || event.which;
+    var _content  = _target.value;
+    if(_key_code === 13) {
+      createListItem(_content);
+    }
+  }
+
+  function createListItem(content) {
+    // <li> 요소 생성
+    var li = document.createElement('li');
+    // <a> 요소 생성
+    li.appendChild( document.createElement('a') );
+    // <button> 요소 생성
+    li.appendChild( document.createElement('button') );
+
+    var link = li.querySelector('a');
+    link.setAttribute('href', '#');
+    link.innerHTML = content;
+    var btn = li.querySelector('button');
+    btn.setAttribute('class', 'remove-item');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-label', 'remove item');
+    btn.innerHTML = 'x';
+    demo_nav.querySelector('ul').appendChild(li);
   }
   // 이벤트 발생 감지 시, 모듈 초기화 함수 실행
   window.onload = initialization;
