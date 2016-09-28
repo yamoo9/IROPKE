a95492112328da656d3e649850882486b9bcb7ce
Jee Hoon <yamoo9@naver.com>
2016-09-28 21:23:15 +0900

    미션 컴플리트!

--- JavaScript_CAMP/DAY07/js/events.js
+++ JavaScript_CAMP/DAY07/js/events.js
@@ -11,68 +11,71 @@
 
   // 모듈 초기화 수행 함수 정의
   function initialization() {
-
     // DOM 요소를 선택
     print_area           = document.querySelector('#print-select-link-content');
     demo_nav             = document.querySelector('.demo-nav');
     demo_nav_links       = demo_nav.querySelectorAll('a');
     demo_nav_remove_btns = demo_nav.querySelectorAll('.remove-item');
-
     // .demo-nav 요소 내부의 <a> 요소에 클릭 이벤트 바인딩
     for( var l=demo_nav_links.length, link; (link = demo_nav_links[--l]); ) {
       link.onclick = printContent;
     }
-
     // .demo-nav 요소 내부의 <button> 요소를 클릭할 때,
     // 선택된 <button>을 포함한 목록 항목(List Item)을 제거한다.
+    Array.prototype.forEach.call(demo_nav_remove_btns, function(button, index) {
+      button.onclick = removeItem;
+    });
   }
-
   // 이벤트 핸들러 printContent 정의
   function printContent() {
-    var _link = this;
-
-    printAssignContent(_link);
-    assignClass(_link);
-
-    // .selected 클래스 속성을 추가 한다.
-    // 단, 각 <a> 요소는 선택된 상태가 고유해야 한다.
-
+    printAssignContent.call(this);
+    removeSiblingsSelectedClass.call(this);
+    assignClass.call(this);
     // 오래 전에 사용되던 브라우저 기본 동작 차단 방법
     return false;
   }
-
-  function printAssignContent(_link) {
-    // 클릭된 <a> 요소가 포함하는 텍스트 노드 값을 가져와서 변수에 참조
-    var _content = _link.firstChild.nodeValue;
-    // #print-select-link-content 요소의 콘텐츠 값이
-    // 선택된 <a> 요소의 콘텐츠 값으로 출력되도록 한다.
+  // #print-select-link-content 요소의 콘텐츠 값이
+  // 선택된 <a> 요소의 콘텐츠 값으로 출력되도록 한다.
+  function printAssignContent() {
+    var _content = this.firstChild.nodeValue;
     print_area.firstChild.nodeValue = _content;
   }
-
-  function assignClass(_link) {
-    // 선택된 <a> 요소 선택된 상태를 시각적으로 표시하도록 설정한다.
-    var _has_selected_class = checkSelectedClass(_link);
-    // console.log('_has_selected_class:', _has_selected_class); // false
+  // 선택된 <a> 요소 선택된 상태를 시각적으로 표시하도록 설정한다.
+  function assignClass() {
+    var _has_selected_class = checkSelectedClass(this);
     if( _has_selected_class ) { return; }
-    var _pre_class = _link.getAttribute('class');
-    _pre_class = _pre_class || '';
+    var _pre_class = this.getAttribute('class') || '';
     var _changed_class = _pre_class + ' selected';
-    _link.setAttribute( 'class', _changed_class.trim() );
+    this.setAttribute( 'class', _changed_class.trim() );
   }
-
-  function checkSelectedClass(_link) {
-    // return selected 클래스가 있다? 없다?;
-    var _classes = _link.getAttribute('class');
+  // return selected 클래스가 있다? 없다?;
+  function checkSelectedClass(el_node) {
+    var _classes = el_node.getAttribute('class');
     _classes = !_classes ? [] : _classes.split(' ');
     var i=0, l=_classes.length;
     for (; i<l; i++) {
-      if ( _classes[i] === 'selected' ) {
-        return true;
-      }
+      if ( _classes[i] === 'selected' ) { return true; }
     }
     return false;
   }
-
+  // .selected 클래스 속성을 추가 한다.
+  // 단, 각 <a> 요소는 선택된 상태가 고유해야 한다.
+  function removeSiblingsSelectedClass() {
+    // 선택된 <a> 요소의 부모 <li>의 형제 <li>를 찾은 후,
+    // 내부의 <a> 요소의 class 속성 값을 확인하여 selected 값을 가지고 있을 경우,
+    // selected 클래스 속성을 포함한 <a> 요소에서 해당 클래스 속성을 제거한다.
+    var _selected_link = this.parentNode.parentNode.querySelectorAll('li a.selected');
+    if ( _selected_link.length > 0 ) {
+      _selected_link = _selected_link[0];
+      var changed_class = _selected_link.getAttribute('class').replace(/selected/, '');
+      _selected_link.setAttribute('class', changed_class);
+    }
+  }
+  // 목록 항목을 제거하는 함수
+  function removeItem() {
+    var _parent = this.parentNode;
+    _parent.parentNode.removeChild(_parent);
+  }
   // 이벤트 발생 감지 시, 모듈 초기화 함수 실행
   window.onload = initialization;
 
