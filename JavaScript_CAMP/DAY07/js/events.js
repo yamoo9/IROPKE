549d61c560dee2e832d994f74f0e45d995484758
Jee Hoon <yamoo9@naver.com>
2016-09-28 19:48:45 +0900

    미션 추가

--- JavaScript_CAMP/DAY07/js/events.js
+++ JavaScript_CAMP/DAY07/js/events.js
@@ -3,6 +3,14 @@
 (function(global){
   'use strict';
 
+  // .demo-nav 요소 내부의 <a> 요소를 클릭할 때,
+  // #print-select-link-content 요소의 콘텐츠 값이
+  // 선택된 <a> 요소의 콘텐츠 값으로 출력되도록 한다.
+  // 선택된 <a> 요소 선택된 상태를 시각적으로 표시하도록 설정한다.
+  // .selected 클래스 속성을 추가 한다.
+  // 단, 각 <a> 요소는 선택된 상태가 고유해야 한다.
 
+  // .demo-nav 요소 내부의 <button> 요소를 클릭할 때,
+  // 선택된 <button>을 포함한 목록 항목(List Item)을 제거한다.
 
 })(this);
