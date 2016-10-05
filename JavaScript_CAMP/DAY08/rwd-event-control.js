baae68abd4f8b2966a34e3fd0c67c9ea22191e0e
Jee Hoon <yamoo9@naver.com>
2016-10-05 21:44:22 +0900

    메모이제이션 패턴 (함수 객체의 속성을 활용)

--- JavaScript_CAMP/DAY08/rwd-event-control.js
+++ JavaScript_CAMP/DAY08/rwd-event-control.js
@@ -17,8 +17,7 @@
     'xl': 1920
   };
 
-  window.onload = init;
-  // window.onresize = detectionResize;
+  global.onload = init;
 
   /** @function initialization */
   function init() {
@@ -27,8 +26,15 @@
     // 1. 문자열 값으로 어떤 기기인지 반환하는 함수
     var device_id = printDetectionDeviceId();
     // 2. 반환된 함수를 <html> 요소의 class 속성에 설정하는 함수
-    // assignClass(document.documentElement, device_id); // DOM Element
-    assignClass('html', device_id);                   // Selector
+    assignClass('html', device_id); // Selector
+    // 3. 사용자의 설정에 따라 창 크기가 변경되면 detectionResize 함수 실행
+    this.onresize = detectionResize;
+    // 4. detectionResize 함수 객체의 memory_class 속성 값에 device_id 값을 복사
+    detectionResize.memory_class = device_id;
+  }
+
+  function query(selector) {
+    return document.querySelector(selector);
   }
 
   /** @function getWindowWidth */
@@ -58,15 +64,39 @@
       ( target_el.nodeType !== 1 && typeof target_el !== 'string' )
     ) { throw new Error('전달인자 유형을 올바르게 확인해주세요.'); }
     if ( typeof target_el === 'string' ) {
-      target_el = document.querySelector(target_el);
+      target_el = query(target_el);
     }
-    var _org_class_name = target_el.getAttribute('class') || '';
-    target_el.setAttribute('class', (_org_class_name + ' ' + class_name).trim() );
+    // 이미 class_name 이름 값을 target_el 소유하고 있다면 함수 종료
+    if ( !hasClass( query('html'), class_name ) ) {
+      var _org_class_name = target_el.getAttribute('class') || '';
+      target_el.setAttribute('class', (_org_class_name + ' ' + class_name).trim() );
+    }
   }
 
+  function hasClass(target_el, class_name) {
+    var _classes = target_el.getAttribute('class').split(' ');
+    for ( var i=0, l=_classes.length; i<l; i++ ) {
+      if( _classes[i] === class_name) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-  // function detectionResize() {
-  //   console.log('detect width:', window.innerWidth);
-  // }
+  function detectionResize() {
+    var device_id = printDetectionDeviceId();
+    // 비교 device_id <-> detectionResize.memory_class 값을 비교
+    console.log('device_id:', device_id);
+    console.log('detectionResize.memory_class:', detectionResize.memory_class);
+    // assignClass('html', device_id);
+    // console.log( 'device_id:', device_id );
+    // console.log( 'detect width:', getWindowWidth() );
+  }
+  // 메모이제이션 패턴
+  // 함수 또한 객체이기 때문에 속성을 가질 수 있다.
+  // 초기 값 설정
+  detectionResize.memory_class = null;
+
+  // console.log(detectionResize.memory_class);
 
 })(this);
