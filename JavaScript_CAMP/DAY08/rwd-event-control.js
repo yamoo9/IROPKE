07ce7a542aa591f7cd7644662642ef878ffe8872
Jee Hoon <yamoo9@naver.com>
2016-10-05 22:06:49 +0900

    코드 리팩토링 (의존 파일[재사용] 분리)

--- JavaScript_CAMP/DAY08/rwd-event-control.js
+++ JavaScript_CAMP/DAY08/rwd-event-control.js
@@ -16,6 +16,8 @@
     'lg': 1440,
     'xl': 1920
   };
+  /** @type {ELEMENT_NODE} <html> 문서 객체 참조 */
+  var root_el = null;
 
   // 로드 이벤트 발생 시, 애플리케이션 초기화
   global.onload = init;
@@ -24,24 +26,15 @@
   function init() {
     // 현재 로드된 후에 사용자 기기의 폭을 감지하여
     // 어떤 디바이스인지 식별하여 처리
+    root_el = query('html');
     // 1. 문자열 값으로 어떤 기기인지 반환하는 함수
     var device_id = printDetectionDeviceId();
     // 2. 반환된 함수를 <html> 요소의 class 속성에 설정하는 함수
-    assignClass('html', device_id); // Selector
+    assignClass( root_el , device_id); // Selector
     // 3. 사용자의 설정에 따라 창 크기가 변경되면 detectionResize 함수 실행
     this.onresize = detectionResize;
     // 4. detectionResize 함수 객체의 memory_class 속성 값에 device_id 값을 복사
     detectionResize.memory_class = device_id;
-  }
-
-  function query(selector) {
-    return document.querySelector(selector);
-  }
-
-  /** @function getWindowWidth */
-  function getWindowWidth() {
-    // innerWidth IE 9+
-    return global.innerWidth || document.body.clientWidth;
   }
 
   /** @function printDetectionDeviceId */
@@ -56,56 +49,13 @@
     return device_id;
   }
 
-  /** @function assignClass */
-  function assignClass(target_el, class_name) {
-    if (
-      // 전달인자 없거나(undefined), null
-      !target_el ||
-      // DOM 요소가 아닌 경우, 문자열이 아닌 경우
-      ( target_el.nodeType !== 1 && typeof target_el !== 'string' )
-    ) { throw new Error('전달인자 유형을 올바르게 확인해주세요.'); }
-    if ( typeof target_el === 'string' ) {
-      target_el = query(target_el);
-    }
-    // 이미 class_name 이름 값을 target_el 소유하고 있다면 함수 종료
-    if ( !hasClass( query('html'), class_name ) ) {
-      var _org_class_name = target_el.getAttribute('class') || '';
-      target_el.setAttribute('class', (_org_class_name + ' ' + class_name).trim() );
-    }
-  }
-
-  function hasClass(target_el, class_name) {
-    var _classes = target_el.getAttribute('class').split(' ');
-    for ( var i=0, l=_classes.length; i<l; i++ ) {
-      if( _classes[i] === class_name) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function removeClass(target_el, class_name) {
-    if ( !class_name ) { target_el.setAttribute('class', ''); }
-    var _class = target_el.getAttribute('class');
-    _class = _class.replace(class_name, ''); // 'name '
-    target_el.setAttribute( 'class', _class.trim() );
-  }
-
   function detectionResize() {
     var device_id = printDetectionDeviceId();
-    // 비교 device_id <-> detectionResize.memory_class 값을 비교
-    // 비교한 값이 달라진다면 기존 클래스 속성 제거 (기억)
-    // 새롭게 변경된 기기의 폭에 따른 식별자를 새 클래스 속성으로 추가
-    // console.log('device_id:', device_id);
-    // console.log('detectionResize.memory_class:', detectionResize.memory_class);
-    // console.log(device_id === detectionResize.memory_class);
-    if ( device_id === detectionResize.memory_class ) {
-      return; // 함수 종료
-    }
+    if ( device_id === detectionResize.memory_class ) { return; }
     // 기억된 클래스 속성 값을 제거 한다.
-    removeClass( query('html') , detectionResize.memory_class);
+    removeClass( root_el , detectionResize.memory_class);
     // 변경된 새 클래스 속성을 <html> 요소의 클래스 속성 값으로 할당한다.
-    assignClass('html', device_id);
+    assignClass( root_el, device_id);
     // 변경된 새 클래스 속성 값을 기억한다.
     detectionResize.memory_class = device_id;
   }
---
+++ JavaScript_CAMP/lib/dom.js
@@ -0,0 +1,36 @@
+function query(selector) {
+  return document.querySelector(selector);
+}
+
+function hasClass(target_el, class_name) {
+  var _classes = target_el.getAttribute('class').split(' ');
+  for ( var i=0, l=_classes.length; i<l; i++ ) {
+    if( _classes[i] === class_name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/** @function assignClass */
+function assignClass(target_el, class_name) {
+  if ( !target_el || target_el.nodeType !== 1 ) { throw new Error('전달인자 유형을 올바르게 확인해주세요.'); }
+  // 이미 class_name 이름 값을 target_el 소유하고 있다면 함수 종료
+  if ( !hasClass( target_el, class_name ) ) {
+    var _org_class_name = target_el.getAttribute('class') || '';
+    target_el.setAttribute('class', trim(_org_class_name + ' ' + class_name) );
+  }
+}
+
+function removeClass(target_el, class_name) {
+  if ( !class_name ) { target_el.setAttribute('class', ''); }
+  var _class = target_el.getAttribute('class');
+  _class = _class.replace(class_name, ''); // 'name '
+  target_el.setAttribute( 'class', trim(_class) );
+}
+
+/** @function getWindowWidth */
+function getWindowWidth() {
+  // innerWidth IE 9+
+  return window.innerWidth || document.body.clientWidth;
+}
