21f5ee787a13b51390099d79bcf277ecb4b26926
Jee Hoon <yamoo9@naver.com>
2016-10-19 22:25:13 +0900

    사용자 정의 객체 생성자와 프로토타입 객체

---
+++ JavaScript_CAMP/DAY10/object.js
@@ -0,0 +1,23 @@
+
+// 자바스크립트 참조(객체) 유형
+// new Function()
+// new Array()
+// new Object()
+// new Number()
+// new String()
+// new Boolean()
+
+// 자바스크립트 원시 데이터 유형
+// string 값(literal)
+// number
+// boolean
+// null
+// undefined
+
+var computer = {}; // Singleton Object
+
+computer.name = 'Apple Computer';
+
+console.log(computer.name); // dot syntax
+
+console.log(computer['name']); // square bracket
