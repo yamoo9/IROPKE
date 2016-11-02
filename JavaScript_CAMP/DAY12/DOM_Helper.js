2d9661b7304b462dba62aa6e225361b02d04f43e
Jee Hoon <yamoo9@naver.com>
2016-11-02 21:49:04 +0900

    $.isEmptyObject()

--- JavaScript_CAMP/DAY12/DOM_Helper.js
+++ JavaScript_CAMP/DAY12/DOM_Helper.js
@@ -33,9 +33,67 @@
   $.isObject = function(data) {
     return _isDataType(data) === 'object';
   };
-  $.isEmptyObject = function(data) {
-    // 속성이 없는 객체인가?
+
+  // ES5 Shim Library
+  if ( !Object.keys ) {
+    Object.keys = function(obj) {
+      var props = [];
+      for ( var prop in obj ) {
+        if ( obj.hasOwnProperty(prop) ) {
+          props.push(prop);
+        }
+      }
+      return props;
+    }
+  }
+
+  $.isEmptyObject = function(obj) {
+    return Object.keys(obj).length === 0;
   };
+
+  // function(obj) {
+  //     // 객체인가?
+  //     if ( !$.isObject(obj) ) {
+  //       throw new Error('전달인자는 객체 유형이어야 합니다.');
+  //     }
+  //     if ( Object.keys ) {
+  //       // ES 5 (IE 9+)
+  //       return Object.keys(obj).length === 0;
+  //     } else {
+  //       // 속성이 없는 객체인가?
+  //       // 속성의 개수를 기억하는 변수
+  //       var props_count = 0;
+  //       // ES 3 (IE 8-)
+  //       // 순환 처리
+  //       for (var prop in obj) {
+  //         // 속성이 있다면 props_count를 증가 시킨다.
+  //         // ※ 객체 자신이 가진 속성만을 골라내야 하기에
+  //         // for ~ in문을 사용할 경우는 반드시~!
+  //         // hasOwnProperty()를 사용하여야 한다.
+  //         if ( obj.hasOwnProperty(prop) ) {
+  //           props_count++;
+  //         }
+  //       }
+  //       return props_count === 0;
+  //     }
+  //   };
+  // $.isEmptyObject = (function(){
+  //   if (Object.keys) {
+  //     return function(obj) {
+  //       return Object.keys(obj).length === 0;
+  //     };
+  //   } else {
+  //     return function(obj) {
+  //       var _props = 0;
+  //       for ( var prop in obj ) {
+  //         if ( obj.hasOwnProperty(prop) ) {
+  //           _props++;
+  //         }
+  //       }
+  //       return _props === 0;
+  //     }
+  //   }
+  // })();
 
 
   // 문서객체 [선택]
