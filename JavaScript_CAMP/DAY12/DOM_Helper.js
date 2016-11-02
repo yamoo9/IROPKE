5fba8db056df42b69089a8723fa3448e0aea7919
Jee Hoon <yamoo9@naver.com>
2016-11-02 22:26:08 +0900

    DOM_Helper 라이브러리 제작 중

--- JavaScript_CAMP/DAY12/DOM_Helper.js
+++ JavaScript_CAMP/DAY12/DOM_Helper.js
@@ -15,29 +15,31 @@
     return toString.call(data).slice(8,-1).toLowerCase();
   };
   // 공개
-  $.isNumber = function(data) {
+  var isNumber = function(data) {
     return _isDataType(data) === 'number';
   };
-  $.isString = function(data) {
+  var isString = function(data) {
     return _isDataType(data) === 'string';
   };
-  $.isBoolean = function(data) {
+  var isBoolean = function(data) {
     return _isDataType(data) === 'boolean';
   };
-  $.isFunction = function(data) {
+  var isFunction = function(data) {
     return _isDataType(data) === 'function';
   };
-  $.isArray = function(data) {
+  var isArray = function(data) {
     return _isDataType(data) === 'array';
   };
-  $.isObject = function(data) {
+  var isObject = function(data) {
     return _isDataType(data) === 'object';
   };
 
-  // 함수를 실행할 때 마다, Object.keys 지원 여부를 물어보는 경우
-  // $.isEmptyObject = function(obj) {
+  ///////////////////////////////////////////////////////
+  // 함수를 실행할 때 마다, Object.keys 지원 여부를 물어보는 경우 //
+  ///////////////////////////////////////////////////////
+  // var isEmptyObject = function(obj) {
   //     // 객체인가?
-  //     if ( !$.isObject(obj) ) {
+  //     if ( !isObject(obj) ) {
   //       throw new Error('전달인자는 객체 유형이어야 합니다.');
   //     }
   //     if ( Object.keys ) {
@@ -64,8 +66,10 @@
 
   // --------------------------------------------------------------
 
-  // IIFE 패턴을 사용하여 코드 분기를 1회만 수행하는 경우
-  // $.isEmptyObject = (function(){
+  ///////////////////////////////////////////////
+  // IIFE 패턴을 사용하여 코드 분기를 1회만 수행하는 경우 //
+  ///////////////////////////////////////////////
+  // var isEmptyObject = (function(){
   //   if (Object.keys) {
   //     return function(obj) {
   //       return Object.keys(obj).length === 0;
@@ -85,34 +89,85 @@
 
   // --------------------------------------------------------------
 
-  // ES5 Shim Library를 사용한 경우
+  /////////////////////////////////
+  // ES5 Shim Library를 사용한 경우 //
+  /////////////////////////////////
   if ( !Object.keys ) {
     Object.keys = function(obj) {
       var props = [];
       for ( var prop in obj ) {
-        if ( obj.hasOwnProperty(prop) ) {
-          props.push(prop);
-        }
+        if ( obj.hasOwnProperty(prop) ) { props.push(prop); }
       }
       return props;
     }
   }
 
-  $.isEmptyObject = function(obj) {
+  var isEmptyObject = function(obj) {
     return Object.keys(obj).length === 0;
   };
 
+  // extend() 함수는 객체의 능력을 합치는 함수
+  var extend = function(o1, o2) {
+    // o1 {} <- o2 {} 합치는 것.
+  };
 
-  // 문서객체 [선택]
-  $.query = function(selector) {
-    if (typeof selector !== 'string') {
+  // 배열 또는 유사 배열(arguments, nodelist)| 객체 순환 콜백 패턴 처리
+  var each = function(data, callback) {
+    // 배열 또는 유사 배열(arguments, nodelist)일 경우 처리
+    if ( !isString(data) && data.length ) {
+      for ( var d, i=0, l=data.length; i<l; i++ ) {
+        d = data[i];
+        // 각각의 원소인 d에 함수를 처리한다.
+        callback.call(d, i, d, data);
+
+      }
+    }
+    // 객체일 경우 처리
+    if ( isObject(data) ) {
+
+    }
+  };
+
+  // --------------------------------------------------------------------------------
+
+  //////////////////
+  // 문서객체 [선택] //
+  //////////////////
+  var query = function(selector) {
+    if ( !isString(selector) ) {
       throw new Error('전달인자는 문자열이어야 합니다.');
     }
     return document.querySelector(selector);
   };
-  $.queryAll = function(selector) {
+  var queryAll = function(selector) {
     return document.querySelectorAll(selector);
   };
 
+  //////////////////
+  // 노출(공개) 패턴 //
+  //////////////////
+
+  // 합성(Mixin) 패턴
+  // $ = extend($, {
+  //   'isNumber'   : isNumber,
+  //   'isString'   : isString,
+  //   'isBoolean'  : isBoolean,
+  //   'isFunction' : isFunction,
+  //   'isArray'    : isArray,
+  //   'isObject'   : isObject,
+  //   'query'      : query,
+  //   'queryAll'   : queryAll,
+  //   'each'       : each
+  // });
+
+  $['isNumber']   = isNumber;
+  $['isString']   = isString;
+  $['isBoolean']  = isBoolean;
+  $['isFunction'] = isFunction;
+  $['isArray']    = isArray;
+  $['isObject']   = isObject;
+  $['query']      = query;
+  $['queryAll']   = queryAll;
+  $['each']       = each;
 
 })(this, (this.DOM_Helper = this.DOM_Helper || {}) );
