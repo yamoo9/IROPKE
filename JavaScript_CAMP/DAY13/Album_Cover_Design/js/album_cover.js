bd520f5bdc2f44067f8367a043b4274d3dbf8e41
Jee Hoon <yamoo9@naver.com>
2016-11-09 21:01:33 +0900

    hasClass, addClass 크로스 브라우징 헬퍼 함수 작성

--- JavaScript_CAMP/DAY13/Album_Cover_Design/js/album_cover.js
+++ JavaScript_CAMP/DAY13/Album_Cover_Design/js/album_cover.js
@@ -2,29 +2,44 @@
 (function(global){
   'use strict';
 
-  function hasClass(el, check_class) {
-    // @el          {element_node}
-    // @check_class {string}
-    var check_class = new RegExp('(^|\\s+)'+ check_class +'(\\s+|$)');
-    return check_class.test( el.getAttribute('class') );
-  }
-  function addClass(el, assign_class) {
-    // 해당 클래스 속성이 이미 존재하면 함수 종료
-    if ( hasClass(el, assign_class) ) { return; }
-    // @el           {element_node}
-    // @assign_class {string}
-    var pre_class = el.getAttribute('class');
-    el.setAttribute('class', pre_class + ' ' + assign_class);
-  }
-  function removeClass() {
+  var hasClass, addClass;
+
+  // 조건 확인: 사용자의 웹 브라우저가 classList를 지원하는가?
+  // classList 지원 브라우저
+  if ( 'classList' in Element.prototype ) {
+
+    hasClass = function(el, check_class) {
+      return el.classList.contains(check_class);
+    };
+
+    addClass = function (el, assign_class) {
+      el.classList.contains(check_class) || el.classList.add(assign_class);
+    };
 
   }
-  function toggleClass() {}
-  // function radioClass() {}
+  // classList 미지원 브라우저
+  else {
 
-  global.hasClass = hasClass;
-  global.addClass = addClass;
-  global.removeClass = removeClass;
+    hasClass = function (el, check_class) {
+      // @el          {element_node}
+      // @check_class {string}
+      var check_class = new RegExp('(^|\\s+)'+ check_class +'(\\s+|$)');
+      return check_class.test( el.getAttribute('class') );
+    };
+
+    addClass = function (el, assign_class) {
+      // 해당 클래스 속성이 이미 존재하면 함수 종료
+      if ( hasClass(el, assign_class) ) { return; }
+      // @el           {element_node}
+      // @assign_class {string}
+      var pre_class = el.getAttribute('class');
+      el.setAttribute('class', pre_class + ' ' + assign_class);
+    }
+
+  }
+
+  global.hasClass    = hasClass;
+  global.addClass    = addClass;
 
 })(this);
 
@@ -50,7 +65,12 @@
     // 디스크 요소에 move-disk 클래스 속성을 추가한다.
     var disk        = evt.target.querySelector('.album-disk');
     var pre_class   = disk.getAttribute('class');
+
+    // helper function (Native)
     addClass(disk, 'move-disk');
+
+    // classList API
+    // disk.classList.add('move-disk');
   }
   function playDisk(evt) {
     // var name = 'playDisk';
