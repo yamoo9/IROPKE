ca8d458da44c3cd8eaede2a61a80bf4144a5979f
Jee Hoon <yamoo9@naver.com>
2016-11-23 19:22:36 +0900

    15주차 수업

---
+++ JavaScript_CAMP/DAY15/README.md
@@ -0,0 +1,5 @@
+###### 이롭게 에이전시 교육
+
+# DAY15
+
+- Carousel UI Component 제작 (Vanilla JavaScript)
---
+++ JavaScript_CAMP/DAY15/UI Carousel/js/ui-carousel.js
@@ -0,0 +1,27 @@
+/*! ui-carousel.js © yamoo9.net, 2016 */
+(function(global){
+  'use strict';
+
+  // -------------------------------------------------------------------------
+  // 문서 객체 참조
+
+  var doc         = global.document;
+  var carousel    = doc.querySelector('.ui-carousel');
+  var tab_wrapper = carousel.querySelector('.ui-carousel-tabpanel-wrapper');
+
+  // -------------------------------------------------------------------------
+  // 이벤트 핸들러
+
+  /** @function resizeCarouselHeight() 캐러셀 컴포넌트 높이를 변경하는 함수 */
+  var resizeCarouselHeight = function() {
+    var tab_panel = tab_wrapper.querySelector('.ui-carousel-tabpanel');
+    carousel.style.height = tab_panel.clientHeight + 'px';
+  };
+
+  // -------------------------------------------------------------------------
+  // 이벤트 핸들링
+
+  global.addEventListener('DOMContentLoaded', resizeCarouselHeight);
+  global.addEventListener('resize', resizeCarouselHeight);
+
+})(this);
