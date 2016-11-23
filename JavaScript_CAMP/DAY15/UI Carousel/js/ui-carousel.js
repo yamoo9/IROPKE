b180ada175b07477a8aa4da62fcf25c7dca1e936
Jee Hoon <yamoo9@naver.com>
2016-11-23 21:07:07 +0900

    캐러셀 인디케이터 이벤트 핸들링

--- JavaScript_CAMP/DAY15/UI Carousel/js/ui-carousel.js
+++ JavaScript_CAMP/DAY15/UI Carousel/js/ui-carousel.js
@@ -1,4 +1,89 @@
 /*! ui-carousel.js © yamoo9.net, 2016 */
+
+// TODO:
+// 0. 캐러셀 탭 패널을 감싼 `래퍼 요소의 너비`를 `탭 패널 너비 × 탭 패널 개수`로 설정한다.
+// 1. 인디케이터 탭 버튼을 누르면 캐러셀 콘텐츠는 해당 콘텐츠를 보여준다.
+// 2. 이전/다음 탐색 버튼을 누르면 캐러셀 콘텐츠는 슬라이드 되어 콘텐츠를 보여준다. (인디케이터 탭 업데이트)
+// 3. 3초마다 자동으로 다음 콘텐츠를 보여줄 수 있도록 설정한다.
+// 4. 마우스가 캐러셀 영역으로 들어가면 자동 넘김이 멈추고, 마우스가 캐러셀 영역 밖으로 나가면 자동 넘김이 다시 시작한다.
+// 5. 인디케이터 탭 또는 이전/다음 탐색 버튼에 포커스가 되면 자동 넘김이 멈춰야 한다.
+// 6. 자동 넘김 또는 넘김 시간 등은 옵션으로 설정할 수 있도록 변경한다.
+// 7. 객체 지향 자바스크립트 방식으로 코드를 변경한다. (e.g: new Carousel('#bs3-headphone') )
+
+(function(global){
+  'use strict';
+
+  // 참조 변수
+  var doc = global.document;
+  // 초기 변수
+  var widget, tablist, tabs, prev_btn, next_btn, tabpanel_wrapper;
+
+  // 컴포넌트 초기화(Component Initialization)
+  var init = function() {
+    // 캐러셀 컴포넌트 객체 참조
+    referenceCarouselControls();
+    // 캐러셀 탭 패널을 감싼 `래퍼 요소의 너비`를 `탭 패널 너비 × 탭 패널 개수`로 설정한다.
+    settingTabpanelWidth();
+    // 이벤트 바인딩(Binding Events)
+    bindEvents();
+  };
+
+
+  // 핸들러 및 함수 정의(Define Event Handlers & Functions)
+  var referenceCarouselControls = function() {
+    widget           = doc.querySelector('.ui-carousel');
+    tablist          = widget.querySelector('.ui-carousel-tablist');
+    tabs             = tablist.querySelectorAll('.ui-carousel-tab');
+    prev_btn         = widget.querySelector('.ui-carousel-prev-button');
+    next_btn         = widget.querySelector('.ui-carousel-next-button');
+    tabpanel_wrapper = widget.querySelector('.ui-carousel-tabpanel-wrapper');
+  };
+  var settingTabpanelWidth = function() {
+    var tabpanels      = tabpanel_wrapper.children;
+    for ( var tabpanel, i=0, l=tabpanels.length; i<l; i++ ) {
+      tabpanel = tabpanels[i];
+      tabpanel.style.width = widget.clientWidth + 'px';
+    }
+    tabpanel_wrapper.style.width = tabpanels[0].clientWidth * l + 'px';
+  };
+  var bindEvents = function() {
+    for(var tab, i=0, l=tabs.length; i<l; i++) {
+      tab = tabs[i];
+      tab.idx = i;
+      tab.onclick = activeTab.bind(tab);
+    }
+  };
+  // 인디케이터 탭 버튼을 누르면 캐러셀 콘텐츠는 해당 콘텐츠를 보여준다.
+  var activeTab = function(e) {
+    e.preventDefault();
+    var distance_x = widget.clientWidth * this.idx * -1;
+    tabpanel_wrapper.style.left = distance_x + 'px';
+    updateIndicator(this);
+  };
+  var updateIndicator = function(activate_tab) {
+    var parent          = activate_tab.parentNode;
+    var parent_siblings = parent.parentNode.children;
+    for ( var sibling, i=0, l=parent_siblings.length; i<l; i++ ) {
+      sibling = parent_siblings[i];
+      if(sibling.classList.contains('active')) {
+        sibling.classList.remove('active');
+        break;
+      }
+    }
+    parent.classList.add('active');
+  };
+
+
+  // 컴포넌트 실행(Excute Component)
+  init();
+
+})(this);
+
+
+// --------------------------------------------------------------------------------
+
+// DOMContentLoaded, resize 이벤트에 따른
+// 캐러셀 컴포넌트 높이 자동 변경 처리 스크립트
 (function(global){
   'use strict';
 
